96167060f65ee9eeb228425811d9fe73665dd926
Lucía Marquez <lucia@soy.com>
2026-09-19 15:27:37 -0300

    second

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const list = document.getElementById("hardware-grid");
+const main = document.querySelector("main");
 
 fetch("./data.json")
  .then((response) => response.json())
  .then((data) => {
-  list.innerHTML = data
-   ?.map(
-    (d) => `
+  const sectors = [...new Set(data.map((act) => act.sector))].sort();
+
+  sectors.forEach((s) => {
+   let div = document.createElement("div");
+   div.classList.add("hero");
+   let h2 = document.createElement("h2");
+   h2.textContent = s;
+   div.appendChild(h2);
+   main.appendChild(div);
+
+   let ul = document.createElement("ul");
+
+   const limit = 35;
+
+   ul.innerHTML = data
+    .filter((d) => d.sector === s)
+    .map(
+     (d) => `
          <a target="_blank" href="//${d.website}"><li>
-           <h3>${d.display_full_name}</h3>
+           <h3>${d.display_full_name.length <= limit ? d.display_full_name : d.display_full_name.slice(0, limit - 3).trim() + "…"}</h3>
            <h4>${d.display_name}</h4>
          </li></a>
        `,
-   )
-   .join("");
- })
- .catch((err) => console.error("Error cargando los componentes:", err));
+    )
+    .join("");
+   main.appendChild(ul);
+  });
+ });
