3a8acafb768c784b6c8ac2c7738801107ddb8e6f
Lucía Marquez <lucia@soy.com>
2026-09-20 23:30:55 -0300

    22

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
 const main = document.querySelector("main");
 
-fetch("./data.json")
- .then((response) => response.json())
- .then((data) => {
-  const sectors = [...new Set(data.map((act) => act.sector))].sort();
+const action = () =>
+ fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+   let dataFiltered;
 
-  sectors.forEach((s) => {
-   let div = document.createElement("div");
-   div.classList.add("hero");
-   let h2 = document.createElement("h2");
-   h2.textContent = s;
-   div.appendChild(h2);
-   main.appendChild(div);
+   if (!localStorage.type || localStorage.type === "one")
+    dataFiltered = data.filter((d) => d.status === "one");
+   else if (localStorage.type === "all") dataFiltered = data;
 
-   let ul = document.createElement("ul");
+   let sectors = [...new Set(dataFiltered.map((act) => act.sector))].sort();
 
-   const limit = 33;
+   main.innerHTML = "";
 
-   ul.innerHTML = data
-    .filter((d) => d.sector === s)
-    .map(
-     (d) => `
+   sectors.forEach((s) => {
+    let div = document.createElement("div");
+    div.classList.add("hero");
+    let h2 = document.createElement("h2");
+    h2.textContent = s;
+    div.appendChild(h2);
+    main.appendChild(div);
+
+    let ul = document.createElement("ul");
+
+    const limit = 33;
+
+    ul.innerHTML = dataFiltered
+     .filter((d) => d.sector === s)
+     .map(
+      (d) => `
          <a target="_blank" href="//${d.website}"><li class="${d.status}">
            <h3>${d.display_full_name.length <= limit ? d.display_full_name : d.display_full_name.slice(0, limit - 3).trim() + "…"}</h3>
            <h4>${d.display_name}</h4>
          </li></a>
        `,
-    )
-    .join("");
-   main.appendChild(ul);
+     )
+     .join("");
+    main.appendChild(ul);
+   });
   });
 
-  const status = [...new Set(data.map((d) => d.status))];
-  console.log(status);
- });
+action();
+
+document.querySelector("#one").onclick = () => {
+ localStorage.setItem("type", "one");
+ action();
+};
+
+document.querySelector("#all").onclick = () => {
+ localStorage.setItem("type", "all");
+ action();
+};
